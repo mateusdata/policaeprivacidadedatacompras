f585ee70a8ef667dd19a717e43ada170174defa9
Mateus Santos <mateuspele2015@gmail.com>
2024-03-03 17:12:15 -0300

    added: comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding:40}}>
+      <h1>Política de Privacidade</h1>
+      <p>
+        Esta Política de Privacidade descreve como o aplicativo Data Compras ("nós", "nosso" ou "aplicativo") coleta, usa e compartilha informações pessoais dos usuários ("você", "seu" ou "usuário") do aplicativo.
+      </p>
+      <h2>Informações Coletadas</h2>
+      <p>
+        O aplicativo Data Compras não coleta informações pessoais dos usuários. O aplicativo é projetado para funcionar localmente no dispositivo do usuário e não requer uma conexão com a internet. Todas as informações inseridas pelo usuário, como itens de compra, são armazenadas localmente no dispositivo do usuário e não são compartilhadas com terceiros.
+      </p>
+      <h2>Uso das Informações</h2>
+      <p>
+        As informações fornecidas pelo usuário são usadas exclusivamente para gerenciar as listas de compras do usuário dentro do aplicativo. Essas informações não são compartilhadas com terceiros nem usadas para fins de marketing.
+      </p>
+      <h2>Segurança</h2>
+      <p>
+        O aplicativo Data Compras emprega medidas de segurança adequadas para proteger as informações do usuário. Todas as informações são armazenadas localmente no dispositivo do usuário e não são transmitidas pela internet.
+      </p>
+      <h2>Alterações nesta Política de Privacidade</h2>
+      <p>
+        Esta Política de Privacidade pode ser atualizada de tempos em tempos. Recomendamos que você revise esta página periodicamente para obter as informações mais recentes sobre nossas práticas de privacidade.
+      </p>
+      <h2>Contate-Nos</h2>
+      <p>
+        Se você tiver alguma dúvida sobre esta Política de Privacidade, entre em contato conosco pelo e-mail: datacompras@example.com.
+      </p>
     </div>
   );
-}
+};
 
 export default App;
